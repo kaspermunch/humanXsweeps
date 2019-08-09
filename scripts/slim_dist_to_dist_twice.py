963a0ca60aa0d5959673848df3b3cb7fd5f25337
kaspermunch <kaspermunch@birc.au.dk>
2019-08-09 11:09:52 +0200

    Tuned memory use

--- scripts/slim_dist_to_dist_twice.py
+++ scripts/slim_dist_to_dist_twice.py
@@ -15,7 +15,7 @@ parser.add_argument("dist_file", type=Path)
 parser.add_argument("dist_twice_file", type=Path)
 args = parser.parse_args()
 
-dist_data = pd.read_hdf(args.dist_file)
+dist_data = pandas.read_hdf(args.dist_file)
 
 individuals, populations, regions = simons_meta_data.get_meta_data(meta_data_dir=analysis_globals.meta_data_dir)
 
@@ -34,6 +34,41 @@ dist_data_twice = (pandas.concat([dist_data[cols],
     .sort_values(['indiv_1', 'start'])
     .reset_index(drop=True)
     )
+
+def optimize_data_frame(df, down_int='integer'):
+    # down_int can be 'unsigned'
+    
+    converted_df = pandas.DataFrame()
+
+    floats_optim = (df
+                    .select_dtypes(include=['float'])
+                    .apply(pandas.to_numeric,downcast='float')
+                   )
+    converted_df[floats_optim.columns] = floats_optim
+
+    ints_optim = (df
+                    .select_dtypes(include=['int'])
+                    .apply(pandas.to_numeric,downcast=down_int)
+                   )
+    converted_df[ints_optim.columns] = ints_optim
+
+    for col in df.select_dtypes(include=['object']).columns:
+        num_unique_values = len(df[col].unique())
+        num_total_values = len(df[col])
+        if num_unique_values / num_total_values < 0.5:
+            converted_df[col] = df[col].astype('category')
+        else:
+            converted_df[col] = df[col]
+
+    unchanged_cols = df.columns[~df.columns.isin(converted_df.columns)]
+    converted_df[unchanged_cols] = df[unchanged_cols]
+
+    # keep columns order
+    converted_df = converted_df[df.columns]      
+            
+    return converted_df
+
+dist_data_twice = optimize_data_frame(dist_data_twice, down_int='unsigned')
 
 dist_data_twice.to_hdf(str(args.dist_twice_file), 'df', 
                            data_columns=['start', 'end', 
