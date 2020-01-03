220c5a4d89c9496e3181962a4505adedcff14036
kaspermunch <kaspermunch@birc.au.dk>
2020-01-03 19:56:40 +0100

    Updated/fixed slim simulations

--- scripts/slim_summary.py
+++ scripts/slim_summary.py
@@ -1,5 +1,5 @@
 
-import os, sys
+import os, sys, re
 import numpy
 import pandas
 from pandas import DataFrame, Series
@@ -36,7 +36,6 @@ args = parser.parse_args()
 # pandas.concat(lst).to_hdf(args.output_file, 'df', format='table', mode='w')
 
 
-
 lst = list()
 for dir_path in args.sweep_data_dir.iterdir():
 
@@ -46,24 +45,46 @@ for dir_path in args.sweep_data_dir.iterdir():
     #sweep_type, pop_size, bottle_start, bottle_end, bottle_pop_size, sweep_start, int(selcoef*100), replication
     simulation, selection_percent, replication =  dir_path.name.rsplit('_', maxsplit=2)
 
-    for file_path in dir_path.glob('*.hdf'):
 
-        pwdist_cutoff = float(file_path.name.split('_')[-1])
+    demography, x_auto_ratio, rec_rate_per_gen, chrom, sweep_type, sweep_start, selcoef, replication = \
+            dir_path.name.split('_')
 
-        sweep_data = pandas.read_hdf(str(file_path))
+    x_auto_ratio = float(x_auto_ratio) / 100
+    rec_rate_per_gen = float(rec_rate_per_gen) / 1e12
+    sweep_start = int(sweep_start)
+    selcoef = float(selcoef) / 100
+    replication = int(replication)
+        
+    for sub_dir_path in dir_path.iterdir():
+            
+        pwdist_cutoff = float(sub_dir_path.name)
+            
+        for file_path in sub_dir_path.glob('clique_data*.hdf'):
+            
+            min_clade_percent = re.search(r'(\d+)%.hdf', file_path.name).group(1)
+         
+            sweep_data = pandas.read_hdf(str(file_path))
 
-        df = (sweep_data
-            .groupby(['start', 'end'])['swept']
-            .aggregate(['sum', 'size'])
-            .rename(columns={'sum': 'nr_swept', 'size': 'total'})
-            .reset_index(level=['start', 'end'])
-            )
-        df['simulation'] = simulation
-        df['replication'] = replication
-        df['selection_coef'] = int(selection_percent) / 100
-        df['pwdist_cutoff'] = pwdist_cutoff
-
-        lst.append(df)
+            sweep_data.to_hdf('test.hdf', 'df', format='table', mode='w')
+            
+            df = (sweep_data
+                    .assign(swept=lambda df: df.swept.astype(int)) # groupby does not reliably sum booleans (return False when less than two are True)
+                    .groupby(['start', 'end'])#['swept']
+                    .swept
+                    .aggregate(['sum', 'size'])
+                    .rename(columns={'sum': 'nr_swept', 'size': 'total'})
+                    .reset_index()
+                    )
+            df['demography'] = demography
+            df['x_auto_ratio'] = x_auto_ratio
+            df['rec_rate_per_gen'] = rec_rate_per_gen
+            df['sweep_start'] = sweep_start
+            df['sweep_type'] = sweep_type
+            df['selcoef'] = selcoef
+            df['replication'] = replication
+            df['min_clade_percent'] = min_clade_percent
+            
+            lst.append(df)
 
 pandas.concat(lst).to_hdf(args.output_file, 'df', format='table', mode='w')
 
--- scripts/slim_trees.py
+++ scripts/slim_trees.py
@@ -9,6 +9,10 @@ import pandas as pd
 from pandas import DataFrame
 import msprime, pyslim
 
+import psutil
+process = psutil.Process(os.getpid())
+
+
 initialization = r'''
 initialize() {
 	initializeTreeSeq();
@@ -113,10 +117,11 @@ parser.add_argument("--recrate", type=float)
 parser.add_argument("--generationtime", type=int)
 parser.add_argument("--sweep", type=str, choices=['partial', 'complete', 'nosweep'])
 parser.add_argument("--sweepstart", type=int)
+parser.add_argument("--xdrive")
 parser.add_argument('--demography', nargs='+', type=str)
 parser.add_argument("--popsize", type=int)
 parser.add_argument("--chrom", type=str, choices=['X', 'A'])
-parser.add_argument("--x-size-reduction", dest='x_size_reduction', type=float)
+parser.add_argument("--size-reduction", dest='size_reduction', type=float)
 parser.add_argument("--totalgenerations", type=int)
 parser.add_argument("--dumpscript", action='store_true')
 #parser.add_argument("slurm_script", type=str)
@@ -142,15 +147,16 @@ slurm_script += finish.replace('TOTAL_GEN', str(args.totalgenerations)).replace(
 
 # add size changes
 for pair in args.demography:
-	gen, size = pair.split(':')
+    gen, size = pair.split(':')
 
-	if args.chrom == 'X':
-		size = str(int(int(size) * args.x_size_reduction))
+    assert int(gen) <= args.totalgenerations
+    
+    size = str(int(int(size) * args.size_reduction))
 
-	if gen == '1':
-		slurm_script += first.replace('GENERATION', gen).replace('SIZE', size)
-	else:		
-		slurm_script += size_change.replace('GENERATION', gen).replace('SIZE', size)
+    if gen == '1':
+        slurm_script += first.replace('GENERATION', gen).replace('SIZE', size)
+    else:
+        slurm_script += size_change.replace('GENERATION', gen).replace('SIZE', size)
 
 # add complete or partial sweep
 if args.sweep == 'partial':
@@ -158,9 +164,11 @@ if args.sweep == 'partial':
 elif args.sweep == 'complete':
     slurm_script += complete_sweep.replace('SWEEP_START', str(args.sweepstart))
 
+
 # make positive selection act on X only in males
-if args.chrom == 'X':
-	slurm_script += drive
+if args.xdrive:
+    assert args.chrom == 'X' and args.sweep != 'nosweep'
+    slurm_script += drive
 
 if 'GWF_JOBID' in os.environ:
 	slurm_script = slurm_script.replace('TMPDIR', '/scratch/' + os.environ['GWF_JOBID'])
@@ -176,6 +184,8 @@ slurm_script_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
 slurm_script_file.write(slurm_script)
 slurm_script_file.close()
 
+print('Before running slim:', process.memory_info().rss/1024**3)
+
 # run slim
 cmd = './slim {}'.format(slurm_script_file.name)
 p = subprocess.Popen(cmd.split(), 
@@ -183,25 +193,66 @@ p = subprocess.Popen(cmd.split(),
 stdout, stderr = p.communicate()
 print(stdout)
 print(stderr)
+# args.trees_file = '/home/kmt/simons/faststorage/people/kmt/steps/slim/simulations/standard/66/4500/X/nosweep/0/0/standard_66_4500_X_nosweep_0_0_6.trees'
 
 # ts = pyslim.load("./sweep.trees").simplify()
 # mutated = msprime.mutate(ts, rate=1e-7, random_seed=1, keep=True)
 # mutated.dump("./sweep_overlaid.trees") 
 
-# load trees from slim
-ts = pyslim.load(args.trees_file)
+# load trees from slim (simplify removes founder invididual in each sub population and thus support for fixed mutations)
+ts = pyslim.load(args.trees_file).simplify()
+
+print('Loaded trees:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
+
+# get nodes for female individuals:
+female_nodes = list()
+for ind in ts.individuals():
+    if pyslim.decode_individual(ind.metadata).sex == 0:
+        female_nodes.extend(ind.nodes)
+
+# prune tree seqeuence to only retain information about the sampled famale nodes:
+subset_ts = ts.simplify(samples=random.sample(female_nodes, k=args.samples))
 
 # overlay mutations
-mutated_ts = msprime.mutate(ts, rate=args.mutationrate*args.generationtime, random_seed=7)
+mutated_ts = msprime.mutate(sample_ts, rate=args.mutationrate*args.generationtime, random_seed=7)
 
-# random indexes for samples
-sample_idx = set(random.sample(range(ts.num_individuals), args.samples))
+# # haplotypes are ordered the same way as samples
+# samples = list(ts.samples())
+# samples_set = set(samples)
+# # so we get the sample indexes of the females to know which haplotypes to consider:
+# famale_haplo_idx = list()
+# for fem in females:
+#     if fem in samples_set:
+#         famale_haplo_idx.append(samples.index(fem))
+
+# # random sample among females:
+# sample_idx = set(random.sample(famale_haplo_idx, k=args.samples))
+
+# print('Computed indexes:', process.memory_info().rss/1024**3)
+# sys.stdout.flush()
+
+# # random indexes for samples
+# #sample_idx = set(random.sample(range(ts.num_individuals), args.samples))
+
+# # overlay mutations
+# mutated_ts = msprime.mutate(ts, rate=args.mutationrate*args.generationtime, random_seed=7)
+
+print('Mutated trees:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
 
 # get the corresponding sample haplotypes
 sample = list()
 for i, hap in enumerate(mutated_ts.haplotypes()):
-    if i in sample_idx:
-        sample.append(hap)
+	if i in sample_idx:
+		print('loading haplotype', i, process.memory_info().rss/1024**3)
+		sys.stdout.flush()
+		sample.append(hap)
+
+assert len(sample) == args.samples
+
+print('Loaded haplotypes:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
 
 # get the positions of each segregating site
 positions = [site.position for site in mutated_ts.sites()]  
@@ -209,11 +260,17 @@ positions = [site.position for site in mutated_ts.sites()]
 # make table with sampled haplotypes
 table = np.array([list(map(np.int8, hap)) for hap in sample]).transpose()
 
+print('Made table:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
+
 # turn table into dataframe with positions
 df = DataFrame(table, dtype='int8')
 df['pos'] = positions
 # write sites to hdf
 df.to_hdf(args.sites_file, 'df', format='table', mode='w')
+
+print('Dumped positions to hdf:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
 
 # add a row with zeros for the start of each window so there is at least
 # one row in each window
@@ -241,6 +298,9 @@ pw_dist_df = (
     .melt(id_vars=['start'], var_name=['indiv_1', 'indiv_2'], value_name='dist')
     )
 
+print('Computed distances:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
+
 # compute proper distance as number of diffs divided by window size
 pw_dist_df['dist'] /= window_size
 
@@ -251,3 +311,6 @@ pw_dist_df.insert(loc=1, column='end', value=pw_dist_df.start + window_size)
 pw_dist_df['indiv_1'] = pw_dist_df['indiv_1'].astype('uint16')
 pw_dist_df['indiv_2'] = pw_dist_df['indiv_2'].astype('uint16')
 pw_dist_df.to_hdf(args.dist_file, 'df', format='table', mode='w')
+
+print('Dumped distances to hdf:', process.memory_info().rss/1024**3)
+sys.stdout.flush()
